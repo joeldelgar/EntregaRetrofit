bf5f99a7c69141d75511db186b530cc30a134fb9
Joel Delgado Garrido <joel.delgado@estudiantat.upc.edu>
2021-10-22 13:13:59 +0200

    retrofit

---
+++ src/main/java/com/vogella/retrofitgerrit/GerritAPI.java
@@ -0,0 +1,14 @@
+package com.vogella.retrofitgerrit;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+import java.util.List;
+
+public interface GerritAPI {
+    @GET("changes/")
+    Call<List<Change>> loadChanges(@Query("q") String status);
+}
+
+
---
+++ src/main/java/com/vogella/retrofitgerrit/Main.java
@@ -0,0 +1,8 @@
+package com.vogella.retrofitgerrit;
+
+public class Main {
+    public static void main(String[] args){
+        Controller controller = new Controller();
+        controller.start();
+    }
+}
